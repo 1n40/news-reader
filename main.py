9f5bff608a949d06bbaec550ff09393e001b14e9
Shreyas Dixit <84516626+SRDdev@users.noreply.github.com>
2021-10-10 18:09:55 +0530

    Corrected the error of News read

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from Googlenews import GoogleNews
-
-news = GoogleNews()
+googlenews =GoogleNews()
+googlenews=GoogleNews(preiod='7d')
+googlenews.search('World')
+result=googlenews.result()
+for x in result :
+  print("-"*50)
+  print("Title--",x['title'])
+  print("Date/Time--",x['date'])
+  print("Description--",x['desc'])
+  print("link--",x['link'])
