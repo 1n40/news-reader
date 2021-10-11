d89559e2317434946c03ad2a76f41b967aa7e4e5
Shreyas Dixit <84516626+SRDdev@users.noreply.github.com>
2021-10-11 10:10:36 +0530

    Added specific Time period and different words to search.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from Googlenews import GoogleNews
-googlenews =GoogleNews()
-googlenews=GoogleNews(preiod='7d')
-googlenews.search('World')
-result=googlenews.result()
+pip install googlenews
+
+x=input("News you want to search ?")
+y=input("The time period you want it to be in")
+from GoogleNews import GoogleNews
+googlenews = GoogleNews()
+googlenews = GoogleNews(lang='en')
+googlenews = GoogleNews(period=y)
+googlenews.search(x)
+result=googlenews.result(sort=True)
 for x in result :
   print("-"*50)
-  print("Title--",x['title'])
-  print("Date/Time--",x['date'])
+  print("Title--      ",x['title'])
+  print("Date/Time--  ",x['date'])
   print("Description--",x['desc'])
-  print("link--",x['link'])
+  print("link--       ",x['link'])
